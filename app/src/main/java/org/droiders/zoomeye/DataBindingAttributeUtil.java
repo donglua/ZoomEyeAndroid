b0deacc3b167d7dd161ba1c6b4b54c6ff7cfae7e
黄东鲁 <foolfalcon@hotmail.com>
2016-04-25 14:02:16 +0800

    修改登录界面

---
+++ app/src/main/java/org/droiders/zoomeye/DataBindingAttributeUtil.java
@@ -0,0 +1,13 @@
+package org.droiders.zoomeye;
+
+/**
+ * Created by Donglua on 16/4/25.
+ */
+public class DataBindingAttributeUtil {
+
+
+
+
+
+
+}
--- app/src/main/java/org/droiders/zoomeye/MainActivity.java
+++ app/src/main/java/org/droiders/zoomeye/MainActivity.java
@@ -1,15 +1,27 @@
 package org.droiders.zoomeye;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.jakewharton.rxbinding.view.RxView;
+import org.droiders.zoomeye.databinding.ActivityMainBinding;
+import rx.android.schedulers.AndroidSchedulers;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
 
+    RxView.clicks(binding.buttonLogin)
+        .debounce(300, MILLISECONDS)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(aVoid -> {
+
+        });
 
   }
 
