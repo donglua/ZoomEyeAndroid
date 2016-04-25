47ff8da094673c7e2ca86d37791048bfe6a718db
黄东鲁 <foolfalcon@hotmail.com>
2016-04-26 01:56:25 +0800

    登录提示

--- app/src/main/java/org/droiders/zoomeye/login/LoginActivity.java
+++ app/src/main/java/org/droiders/zoomeye/login/LoginActivity.java
@@ -8,21 +8,20 @@ import com.devspark.appmsg.AppMsg;
 import com.jakewharton.rxbinding.view.RxView;
 import javax.inject.Inject;
 import org.droiders.zoomeye.R;
-import org.droiders.zoomeye.databinding.ActivityMainBinding;
+import org.droiders.zoomeye.databinding.ActivityLoginBinding;
 import org.droiders.zoomeye.di.AppModule;
 import rx.android.schedulers.AndroidSchedulers;
-import timber.log.Timber;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class LoginActivity extends AppCompatActivity implements LoginContract.View {
 
-  private ActivityMainBinding binding;
+  private ActivityLoginBinding binding;
   @Inject LoginPresenter mPresenter;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
     LoginComponent loginComponent = DaggerLoginComponent.builder()
         .loginModule(new LoginModule(this))
@@ -40,20 +39,20 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
   public void loginClick() {
     final String email    = binding.etEmail.getText().toString();
     final String password = binding.etPassword.getText().toString();
-    Timber.d("email = " + email);
     if (TextUtils.isEmpty(email)) {
-      AppMsg.makeText(this, "邮件地址不能为空~", AppMsg.STYLE_ALERT).show();
+      AppMsg.makeText(this, R.string.email_not_null, AppMsg.STYLE_ALERT).show();
       return;
     }
     if (TextUtils.isEmpty(password)) {
-      AppMsg.makeText(this, "密码不能为空~", AppMsg.STYLE_ALERT).show();
+      AppMsg.makeText(this, R.string.password_not_null, AppMsg.STYLE_ALERT).show();
       return;
     }
     mPresenter.login(email, password);
   }
 
   @Override public void loginSuccess() {
-    AppMsg.makeText(this, "登录成功", AppMsg.STYLE_INFO).show();
+    AppMsg.makeText(this, getString(R.string.login_success), AppMsg.STYLE_INFO).show();
+
 
   }
 
